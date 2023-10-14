555230c76f88cb4b04033b481a11e927a049ce4f
RRRYOOO <damdagqtjp@gmail.coom>
2023-10-14 18:59:52 +0900

    create osbook_05c

---
+++ mikanOS/day05/osbook_05c/source/kernel/font.cpp
@@ -0,0 +1,29 @@
+#include "font.hpp"
+
+extern const uint8_t _binary_hankaku_bin_start;   // フォントデータの開始アドレス
+extern const uint8_t _binary_hankaku_bin_end;     // フォントデータの終了アドレス+1
+extern const uint8_t _binary_hankaku_bin_size;    // フォントデータのサイズ
+
+/* 指定した文字のフォントデータの格納先アドレスを返す関数 */
+const uint8_t* GetFont(char c) {
+  auto index = 16 * static_cast<unsigned int>(c);   // 引数cの文字をASCIIコードに変換後に、×16してフォントデータの先頭アドレスを算出
+  if (index >= reinterpret_cast<uintptr_t>(&_binary_hankaku_bin_size)) {  // indexの値がフォントデータの全サイズより大きい場合（エラー）
+    return nullptr;
+  }
+  return &_binary_hankaku_bin_start + index;    // フォントデータの開始アドレスからindexだけ進んだアドレスを返す
+}
+
+/* 'A'のフォント描画用の関数 */
+void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color) {
+  const uint8_t* font = GetFont(c);   // 指定した文字のフォントデータの格納先アドレスを取得
+  if (font == nullptr) {    // アドレス読み出し失敗
+    return;
+  }
+  for (int dy = 0; dy < 16; ++dy) {
+    for (int dx = 0; dx < 8; ++dx) {
+      if (font[dy] << dx & 0x80u) {
+        writer.Write(x + dx, y + dy, color);
+      }
+    }
+  }
+}
